0480e8ecf7afeb49e5ce85b9cc245c5ff029e8d8
Niki VM 42 <nik.7377@gmail.com>
2024-03-16 20:34:15 +0100

    link test about page

--- tests/test_example.py
+++ tests/test_example.py
@@ -7,11 +7,9 @@ def test_has_title(page: Page):
     expect(page).to_have_title(re.compile("Home"))
 
 def test_get_started_link(page: Page):
-    page.goto("https://playwright.dev/")
+    page.goto("http://127.0.0.1:8000")
 
     # Click the get started link.
-    page.get_by_role("link", name="Get started").click()
-
+    page.get_by_role("link", name="About page").click()
     # Expects page to have a heading with the name of Installation.
-    expect(page.get_by_role("heading", name="Installation")).to_be_visible()
-
+    expect(page).to_have_title(re.compile("About"))
