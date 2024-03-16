09fc83701fe65b3c7292f19733977fb980cf9d1f
Niki VM 42 <nik.7377@gmail.com>
2024-03-16 21:17:33 +0100

    add styles.css

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,13 +3,23 @@ from playwright.sync_api import Page, expect
 
 def test_has_title(page: Page):
     page.goto("http://127.0.0.1:8000")
-
     expect(page).to_have_title(re.compile("Home"))
 
-def test_get_started_link(page: Page):
+
+def test_link_about(page: Page):
     page.goto("http://127.0.0.1:8000")
 
     # Click the get started link.
     page.get_by_role("link", name="About page").click()
     # Expects page to have a heading with the name of Installation.
     expect(page).to_have_title(re.compile("About"))
+
+def id_click_and_expect(page:Page, element_id:str, expect_site:str):
+    page.locator(element_id).click()
+    expect(page).to_have_title(re.compile(expect_site))
+
+def test_nav_list(page: Page):
+    page.goto("http://127.0.0.1:8000")
+
+    id_click_and_expect(page, "#nav_about", "About")
+    id_click_and_expect(page, "#nav_home", "Home")
