8b2d53b70d748109b757a880b40086e46d6c4f5c
niki <nik.7377@gmail.com>
2024-03-27 13:06:45 +0100

    hello

--- board/__init__.py
+++ board/__init__.py
@@ -42,4 +42,4 @@ def create_app():
 
 if __name__ == "__main__":
     app, socketio = create_app()
-    socketio.run(app, host='0.0.0.0', port=8001, debug=True)
+    socketio.run(app, host='0.0.0.0', port=8001, debug=True, allow_unsafe_werkzeug=True)
