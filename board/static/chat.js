8ec1f105ca968941eb6df184af5aa22500ec0682
niki <nik.7377@gmail.com>
2024-03-28 18:39:11 +0100

    split it

--- board/static/chat.js
+++ board/static/chat.js
@@ -57,60 +57,6 @@ function joinRoom() {
     socket.emit('join', {room: room});
 }
 
-/* socket.on('connect', (data) => {
-    emit("connect")
-}); */
-
-function resetButtonColor(room) {
-    document.getElementById("header-room").innerText = room;
-    const other_buttons = document.getElementsByClassName("room-buttons");
-    for (let i = 0; i < other_buttons.length; i++) {
-        other_buttons[i].classList.remove("btn-success");
-        other_buttons[i].classList.add("btn-primary");
-    }
-}
-
-function switchChannel(room, rooms_buttons) {
-    resetButtonColor(room);
-    rooms_buttons.classList.remove("btn-primary");
-    rooms_buttons.classList.add("btn-success");
-    addChannelBox(room);
-}
-
-function addChannelButton(room) {
-    const rooms_buttons = document.createElement('button');
-    rooms_buttons.innerText=room
-    rooms_buttons.classList.add("btn", "btn-success", "room-buttons")
-    resetButtonColor(room)
-    rooms_buttons.addEventListener("click", function() {
-        switchChannel(room, rooms_buttons);
-    });
-    document.getElementById("rooms-id").appendChild(rooms_buttons);
-}
-
-function addChannelBox(room) {
-    const old_room = document.getElementById("chat-box-id-" + current_room);
-    if (old_room) {
-        old_room.style.display = 'none';
-    }
-    const new_room = document.getElementById("chat-box-id-" + room);
-    if (new_room) {
-        new_room.style.display = 'block';
-        current_room=room;
-        return;
-    }
-    const new_div = document.createElement('div');
-    new_div.classList.add("chat-box");
-    new_div.setAttribute("id", "chat-box-id-" + room);
-    document.getElementById("header-room-div").after(new_div);
-    current_room=room;
-}
-
-function addChannel(room) {
-    addChannelBox(room);
-    addChannelButton(room);
-}
-
 socket.on('addChannelToFrontend', (room) => {
     addChannel(room)
 });
--- board/static/createChannels.js
+++ board/static/createChannels.js
@@ -1,59 +1,49 @@
-/* class createChannels {
-  
-  constructor(room, current_room) {
-    this.room = room;
-    this.current_room = current_room;
-  }
+function addChannelButton(room) {
+  const rooms_buttons = document.createElement('button');
+  rooms_buttons.innerText=room
+  rooms_buttons.classList.add("btn", "btn-success", "room-buttons")
+  resetButtonColor(room)
+  rooms_buttons.addEventListener("click", function() {
+      switchChannel(room, rooms_buttons);
+  });
+  document.getElementById("rooms-id").appendChild(rooms_buttons);
+}
 
-  resetButtonColor() {
-    document.getElementById("header-room").innerText = this.current_room;
-    const other_buttons = document.getElementsByClassName("room-buttons");
-    for (let i = 0; i < other_buttons.length; i++) {
+function resetButtonColor(room) {
+  document.getElementById("header-room").innerText = room;
+  const other_buttons = document.getElementsByClassName("room-buttons");
+  for (let i = 0; i < other_buttons.length; i++) {
       other_buttons[i].classList.remove("btn-success");
       other_buttons[i].classList.add("btn-primary");
-    }
   }
+}
 
-  switchChannel(rooms_buttons) {
-    this.resetButtonColor();
-    rooms_buttons.classList.remove("btn-primary");
-    rooms_buttons.classList.add("btn-success");
-    this.addChannelBox();
+function switchChannel(room, rooms_buttons) {
+  resetButtonColor(room);
+  rooms_buttons.classList.remove("btn-primary");
+  rooms_buttons.classList.add("btn-success");
+  addChannelBox(room);
+}
+
+function addChannelBox(room) {
+  const old_room = document.getElementById("chat-box-id-" + current_room);
+  if (old_room) {
+      old_room.style.display = 'none';
   }
-
-  addChannelButton() {
-    const rooms_buttons = document.createElement('button');
-    rooms_buttons.innerText=this.room
-    rooms_buttons.classList.add("btn", "btn-success", "room-buttons")
-    this.resetButtonColor()
-
-    const self = this;
-    rooms_buttons.addEventListener("click", function() {
-      self.switchChannel(rooms_buttons);
-    });
-    document.getElementById("rooms-id").appendChild(rooms_buttons);
+  const new_room = document.getElementById("chat-box-id-" + room);
+  if (new_room) {
+      new_room.style.display = 'block';
+      current_room=room;
+      return;
   }
+  const new_div = document.createElement('div');
+  new_div.classList.add("chat-box");
+  new_div.setAttribute("id", "chat-box-id-" + room);
+  document.getElementById("header-room-div").after(new_div);
+  current_room=room;
+}
 
-  addChannelBox() {
-    const old_room = document.getElementById("chat-box-id-" + this.current_room);
-    if (old_room) {
-        old_room.style.display = 'none';
-    }
-    const new_room = document.getElementById("chat-box-id-" + this.room);
-    if (new_room) {
-        new_room.style.display = 'block';
-        return this.room;
-    }
-    const new_div = document.createElement('div');
-    new_div.classList.add("chat-box");
-    new_div.setAttribute("id", "chat-box-id-" + this.room);
-    document.getElementById("header-room-div").after(new_div);
-    return this.room;
-  }
-
-  addChannel() {
-    this.current_room = this.addChannelBox();
-    this.addChannelButton();
-    return this.current_room;
-  }
-} */
+function addChannel(room) {
+  addChannelBox(room);
+  addChannelButton(room);
+}
